87ed87a705ad5c1f71eb5c8984ff387d4e92885d
bladewizz <rajkapadia0@gmail.com>
2026-08-21 15:15:48 -0400

    fixed second test issue

--- tests/recorded.spec.ts
+++ tests/recorded.spec.ts
@@ -2,9 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  await page.getByRole('link', { name: 'Node.js' }).click();
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByRole('link', { name: 'Model Context Protocol' }).click();
-  const page1 = await page1Promise;
-  await page1.locator('a').filter({ hasText: 'Understand conceptsLearn the' }).click();
+  await page.getByRole('button', { name: 'Node.js' }).click();
 });
