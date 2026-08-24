ad2faa0b95c581cba7627da564bde960e6a31eeb
bladewizz <rajkapadia0@gmail.com>
2026-08-24 17:37:28 -0400

    complete 2.3 objects and nested access

---
+++ scratch/objects.js
@@ -0,0 +1,12 @@
+const account = {
+  name: "Acme Corp",
+  industry: "Manufacturing",
+  annualRevenue: 5000000,
+  billingAddress: {
+    city: "Atlanta",
+    state: "GA"
+  }
+};
+
+console.log(account.industry);
+console.log(account.billingAddress.city);
