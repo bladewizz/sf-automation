3c0818e2a543d12cce92c434e41bdfa61f8788d0
bladewizz <rajkapadia0@gmail.com>
2026-08-24 18:02:18 -0400

    2.6 promises

---
+++ scratch/promises.js
@@ -0,0 +1,15 @@
+console.log("first");
+
+setTimeout(() => {
+  console.log("second - two seconds later");
+}, 2000);
+
+console.log("third");
+
+const order = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("burger is ready");
+  }, 2000);
+});
+
+order.then((result) => console.log(result));
